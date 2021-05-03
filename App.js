228ef0607d5b3441fdc548b631bf34f3d993d10b
Sajan491 <sajanmahat491@gmail.com>
2021-05-03 08:03:42 +0545

    modified file structuring

--- App.js
+++ App.js
@@ -1,74 +1,16 @@
 import React from 'react'
-
-import 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image} from 'react-native';
-import DrawerNavigator from "./routes/DrawerNavigator";
-
-//import screens
-import LoginScreen from "./screens/LoginScreen";
-import SplashScreen from "./screens/SplashScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-
-const Stack = createStackNavigator()
-
-const Auth = () => {
-  //Stack Navigator for the Login and Register Screen
-  return(
-    <Stack.Navigator initialRouteName="LoginScreen">
-      <Stack.Screen 
-        name = "LoginScreen"
-        component = {LoginScreen}
-        options = {{headerShown: false}}
-      />
-      <Stack.Screen 
-        name = "RegisterScreen"
-        component = {RegisterScreen}
-        options = {{title: "Register", 
-        headerStyle: {
-          backgroundColor: "#307ecc"
-        },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-        }}
-      />
-    </Stack.Navigator>
-  )
-}
+import AppNavigator from "./app/navigation/AppNavigator";
+import navigationTheme from './app/navigation/navigationTheme';
+import {NavigationContainer} from "@react-navigation/native"
 
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName ="SplashScreen">
-        <Stack.Screen 
-          name = "SplashScreen"
-          component = {SplashScreen}
-          options = {{headerShown: false}}
-        />
-        <Stack.Screen 
-          name = "Auth"
-          component = {Auth}
-          options = {{headerShown: false}}
-        />
-          <Stack.Screen 
-          name = "DrawerNavigator"
-          component = {DrawerNavigator}
-          options = {{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return(
+    <NavigationContainer theme={navigationTheme}>
+      
+      <AppNavigator />
+    </NavigationContainer>
+    )
+  
+}
---
+++ app/navigation/FeedNavigator.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import React from "react"
+import PostDetailScreen from "../screens/PostDetailScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const FeedNavigator=()=>(
+    <Stack.Navigator mode="modal">
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        <Stack.Screen name="PostDetails" component={PostDetailScreen}/>
+    </Stack.Navigator>
+)
+
+export default FeedNavigator;
