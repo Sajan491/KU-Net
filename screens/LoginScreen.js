d3e4217e231195b1113979260138fb467a1a62da
Sajan Mahat <52095768+Sajan491@users.noreply.github.com>
2021-05-03 12:15:17 +0545

    Merging Main to Sajan (#4)
    * User Authentication Setup + Webpack.config.js for native-base
    
    * login register layout
    
    Co-authored-by: Sabin Thapa <sabint017@gmail.com>

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,34 +1,73 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, {useState} from 'react';
+import { View, Button, Text, StyleSheet, Image } from 'react-native';
+
+import {Container, Content, Header, Input, Form, Item, Label} from "native-base"
+import firebase from "firebase";
 
 const LoginScreen = ({navigation}) => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    const loginUser = (email, password) => {
+        console.log(email)
+        try{
+            firebase.auth().signInWithEmailAndPassword(email, password).then((user) => console.log(user))
+        }
+        catch(err){
+            console.log(err.toString())
+        }
+    }
+    
 
     const pressHandlerRegister = () => {
         navigation.navigate("Register")
     }
-
+    
     const pressHandlerHome = () => {
         navigation.navigate("After")
     }
 
     return (
-        <View style ={styles.container}>
-            <Text>Login Screen | KU Net</Text>
-            <Text > Don't have an account? </Text>
-            <View style={{margin: 40}}> 
-            <Button title="Login" onPress={pressHandlerHome} />
-            </View>
-           
+        <>
+            <Container style={styles.container}>
+                <Form>
+                    <Item floatingLabel>
+                        <Label> Email</Label>
+                        <Input autoCapitalize="none" autoCorrect={false} onChangeText={(email) => setEmail(email)}/>
+                    </Item>
+                    <Item floatingLabel>
+                        <Label> Password</Label>
+                        <Input secureTextEntry={true} autoCapitalize="none" autoCorrect={false} onChangeText={(password) => setPassword(password)}/>
+                    </Item>
+                    <View style={styles.LoginBtn}>
+                        <Button title="Login" onPress={()=>loginUser(email, password)}  />    
+                    </View>
+                </Form>
+            <View style= {styles.Register}>
+            <Text style={{marginTop:40, marginLeft: 150}}> Don't have an account? </Text>
+            <View style={{margin: 10}}> 
             <Button title="Register" onPress={pressHandlerRegister}/>
-        </View>
+            </View>
+
+            </View>
+            </Container>
+            
+        </>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        padding: 50,
-        alignItems: "center",
+        flex: 1,
+        padding: 10,
         justifyContent: "center"
+    },
+    LoginBtn: {
+        marginTop: 30
+    },
+    Register: {
+        marginBottom: 0
     }
 })
 export default LoginScreen
