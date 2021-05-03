f55ad91e66d82ec3266ffedf95fa61a4b63a5961
Sajan491 <sajanmahat491@gmail.com>
2021-05-03 09:14:09 +0545

    postdetails screen done

--- app/screens/PostDetailScreen.js
+++ app/screens/PostDetailScreen.js
@@ -1,14 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import AppText from '../components/AppText'
 
-const PostDetailScreen = () => {
+const PostDetailScreen = ({route}) => {
+    const post=route.params;
     return (
         <View>
-            <Text>Post Detail</Text>
+            <Image style={styles.image} source={post.image} />
+            <View style={styles.detailsContainer}>
+                <AppText style={styles.title}>{post.title}</AppText>
+            </View>
         </View>
     )
 }
 
 export default PostDetailScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image:{
+        width:"100%",
+        height: 300
+    },
+    detailsContainer:{
+        padding:20
+    },
+
+    
+    title:{
+        marginBottom:7,
+        fontSize:16,
+        fontWeight:'bold'
+    }
+})
