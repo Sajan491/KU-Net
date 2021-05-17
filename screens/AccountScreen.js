02d2ac1951b2948c3820902b3fb9a1adde7f4474
= <=>
2021-05-17 11:06:16 +0545

    temp soln for account page refresh

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -1,11 +1,13 @@
-import React, {useContext} from 'react'
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React, {useContext, useState, useEffect, useCallback} from 'react'
+import { FlatList, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native'
 import colors from "../config/colors";
 import Screen from "../components/Screen";
 import ListItem from '../components/ListItem';
 import ItemSeperator from '../components/ItemSeperator';
 import MyIcon from "../components/MyIcon";
 import {AuthContext} from "../context/AuthProvider"
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
 const menuItems = [
     {
         title:"My Posts",
@@ -34,43 +36,76 @@ const menuItems = [
     }
 ]
 
+const delay = (timeout)  => {
+    return new Promise(resolve => {
+        setTimeout(resolve, timeout)
+    })
+}
+
+
+
 const AccountScreen = ({navigation}) => {
     const {user, signOut} = useContext(AuthContext);
-    console.log(user.displayName);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+    }, [user.displayName])
+
+    
+
+    const loadMore = useCallback(async () => {
+        setLoading(true);
+
+        delay(500).then(() => setLoading(false))
+    }, [loading])
+
+    console.log("Username: " ,user.displayName);
+
     return (
         <Screen style={styles.screen}>
-            <View style={styles.container}>
-                <ListItem 
-                    title= {user.displayName ? user.displayName : "Update name in credentials!"}
-                    subTitle= {user.email}
-                    image={require('../assets/sajan.png')}
-                />
-            </View>
-            <View style={styles.container}>
-                <FlatList 
-                    data={menuItems}
-                    keyExtractor={(menuItem)=> menuItem.title}
-                    ItemSeparatorComponent={ItemSeperator}
-                    renderItem={({item})=>(
-                        <ListItem 
-                            title={item.title}
-                            IconComponent={
-                                <MyIcon name={item.icon.name} backgroundColor={item.icon.backgroundColor}/>
-                            }
-                            onPress={()=>navigation.navigate(item.targetScreen)}
-                        />
-                    )}
-                />
-            </View>
-            <View>
-                <ListItem 
-                    title="Log Out"
-                    IconComponent={
-                        <MyIcon name='logout' backgroundColor='#ffe66d'/>
-                    }
-                    onPress={()=>signOut()}
-                />
-            </View>
+            <ScrollView 
+                refreshControl = {
+                    <RefreshControl
+                    progressBackgroundColor= {colors.primary}
+                    tintColor={colors.secondary}
+                    refreshing={loading}
+                    onRefresh={loadMore}
+                  />
+                }
+            >
+                <View style={styles.container}>
+                    <ListItem 
+                        title= {user.displayName ? user.displayName : "Update name in credentials!"}
+                        subTitle= {user.email}
+                        image={require('../assets/sajan.png')}
+                    />
+                </View>
+                <View style={styles.container}>
+                    <FlatList 
+                        data={menuItems}
+                        keyExtractor={(menuItem)=> menuItem.title}
+                        ItemSeparatorComponent={ItemSeperator}
+                        renderItem={({item})=>(
+                            <ListItem 
+                                title={item.title}
+                                IconComponent={
+                                    <MyIcon name={item.icon.name} backgroundColor={item.icon.backgroundColor}/>
+                                }
+                                onPress={()=>navigation.navigate(item.targetScreen)}
+                            />
+                        )}
+                    />
+                </View>
+                <View>
+                    <ListItem 
+                        title="Log Out"
+                        IconComponent={
+                            <MyIcon name='logout' backgroundColor='#ffe66d'/>
+                        }
+                        onPress={()=>signOut()}
+                    />
+                </View>
+            </ScrollView>
         </Screen>
     )
 }
