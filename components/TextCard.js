5b12608c11c1dfb2ed5e9fb3ef1f9d3eae226afc
= <=>
2021-05-26 20:58:30 +0545

    Email/Password change backend+frontend

--- components/TextCard.js
+++ components/TextCard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { StyleSheet, View, Button, TouchableOpacity } from 'react-native'
 import AppText from "../components/AppText"
-import AppButton from "../components/AppButton";
 import colors from '../config/colors'
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 const TextCard = ({title, subTitle, description, onPress}) => {
     return (
         <View style = {styles.card}>
             <AppText style = {styles.title}>{title}</AppText>
             <AppText style = {styles.subTitle}>{subTitle}</AppText>
             <AppText style = {styles.description}>{description}</AppText>
-            <AppButton title="Change" color="secondary" onPress = {onPress} style = {styles.btn} />
+            <MaterialCommunityIcons name="circle-edit-outline" size={24} onPress = {onPress} style={styles.editBtn} />
         </View>
     )
 }
@@ -18,7 +18,7 @@ export default TextCard
 
 const styles = StyleSheet.create({
     card: {
-        padding: 10,
+        padding: 20,
         borderRadius: 15,
         backgroundColor: colors.white,
         overflow: 'hidden',
@@ -36,10 +36,9 @@ const styles = StyleSheet.create({
         fontSize: 12,
         color: "gray"
     },
-    btn: {
-        display: "flex",
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
-        width: "20%"
+    editBtn: {
+        position: "absolute",
+        right: 30,
+        top: 12
     }
 })
--- screens/AccountSettings.js
+++ screens/AccountSettings.js
@@ -1,28 +1,153 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Modal, TouchableOpacity, TextInput, Alert } from 'react-native'
 import Screen from "../components/Screen";
 import firebase from "../config/firebase";
-import AppButton from "../components/AppButton";
+import AppText from "../components/AppText";
 import TextCard from "../components/TextCard";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import colors from '../config/colors';
 
 
 const AccountSettings = () => {
-   const changePasswordHandler = () => {
-       console.log("Password Changed!");
+    const [modalOpen, setModalOpen] = useState(false);
+    const [currentPassword, setCurrentPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
+    const [reNewPassword, setreNewPassword] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+    const [actionTriggered, setActionTriggered] = useState("")
+    var user = firebase.auth().currentUser;
+
+    const reAuthenticateUser = (currentPassword) =>{
+        const cred = firebase.auth.EmailAuthProvider.credential(user.email, currentPassword);
+        return user.reauthenticateWithCredential(cred);
+    }
+
+    const changePasswordHandler = () => {
+        if(newPassword == reNewPassword) {
+            reAuthenticateUser(currentPassword).then(() => {
+                user.updatePassword(newPassword).then((res) => {
+                   Alert.alert("Password successfully changed!");
+                   setModalOpen(false);
+                   setCurrentPassword("");
+                   setreNewPassword("");
+                   setNewPassword("");
+                }).catch((err) => {
+                    Alert.alert(err.message);
+                }) 
+    
+            }).catch((err) => {
+                Alert.alert(err.message)
+            })
+
+        }else {
+            Alert.alert("The new passwords do not match!")
+        }
+
    }
 
    const changeEmailHandler = () => {
-    console.log("Email Changed!");
-   }
+    reAuthenticateUser(currentPassword).then(() => {
+        user.updateEmail(newEmail).then((res) => {
+           Alert.alert("Email successfully changed!");
+           setModalOpen(false);
+            setNewEmail("");
+        }).catch((err) => {
+            Alert.alert(err.message);
+        }) 
+
+    }).catch((err) => {
+        Alert.alert(err.message)
+    })
+
+}
 
     return (
+        
         <Screen>
-            {/* <AppText> Account Settings </AppText>
-            <AppText>{firebase.auth().currentUser.email}</AppText> */}
-            <TextCard title ="Email Address:" subTitle = {firebase.auth().currentUser.email} onPress= {changeEmailHandler} />
+            <Modal 
+                visible = {modalOpen} 
+                animationType = "slide"
+                onRequestClose = {() => setModalOpen(false)}
+                >
+                <MaterialCommunityIcons
+                    name = "close"
+                    size = {26}
+                    style = {styles.modalClose}
+                    onPress = {() => setModalOpen(false)}
+                />
+                {actionTriggered == "PasswordModal" ? 
+                    <View>
+                        <AppText style = {styles.header}> Set up new password.</AppText>
+                        <TextInput 
+                            style = {styles.textInput}
+                            value = {currentPassword}
+                            placeholder = "Current Password..."
+                            autoCapitalize = "none"
+                            secureTextEntry = {true}
+                            onChangeText = {(pwd) => setCurrentPassword(pwd)}
+                        />
+                        <TextInput 
+                            style = {styles.textInput}
+                            value = {newPassword}
+                            placeholder = "New Password..."
+                            autoCapitalize = "none"
+                            secureTextEntry = {true}
+                            onChangeText = {(pwd) => setNewPassword(pwd)}
+                        />
+                        <TextInput 
+                            style = {styles.textInput}
+                            value = {reNewPassword}
+                            placeholder = "Re-type New Password..."
+                            autoCapitalize = "none"
+                            secureTextEntry = {true}
+                            onChangeText = {(pwd) => setreNewPassword(pwd)}
+                        />
+                        <TouchableOpacity onPress = {() =>  {changePasswordHandler()} } >
+                            <Text style = {styles.btnText}> Change Password</Text>
+                        </TouchableOpacity>
+
+                    </View>
+                    :
+                    <View>
+                        <AppText style = {styles.header}> Set up new email.</AppText>
+                        <TextInput 
+                            style = {styles.textInput}
+                            value = {currentPassword}
+                            placeholder = "Current Password..."
+                            autoCapitalize = "none"
+                            secureTextEntry = {true}
+                            onChangeText = {(pwd) => setCurrentPassword(pwd)}
+                        />
+                        <TextInput 
+                            style = {styles.textInput}
+                            value = {newEmail}
+                            placeholder = "New Email..."
+                            autoCapitalize = "none"
+                            onChangeText = {(email) => setNewEmail(email)}
+                        />
+                        <TouchableOpacity onPress = {() =>  {changeEmailHandler()} } >
+                            <Text style = {styles.btnText}> Change Email</Text>
+                        </TouchableOpacity>
+
+                </View>
+}
+            </Modal>
+          
+            <TextCard title ="Email Address:" subTitle = {firebase.auth().currentUser.email} onPress= {() => {
+                setModalOpen(true);
+                setActionTriggered("EmailModal")
+             }
+            } 
+            />
          
 
-            <TextCard title ="Change Password:" description = "Password must be atleast 6 characters long." onPress = {changePasswordHandler}/>
+            <TextCard title ="Change Password:" description = "Password must be atleast 6 characters long." onPress = {() => {
+                setModalOpen(true)
+                setActionTriggered("PasswordModal")    
+                }
+            }
+            />
+                
          
 
         </Screen>
@@ -31,4 +156,29 @@ const AccountSettings = () => {
 
 export default AccountSettings
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    modalClose: {
+        padding: 15,
+        alignSelf: "flex-end",
+        marginBottom: 15,
+    },
+    textInput: {
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        height: 40,
+        borderWidth: 1,
+        borderColor: "gray",
+        marginVertical: 10
+    },
+    btnText: {
+        marginVertical: 10,
+        fontSize: 16,
+        color: colors.secondary,
+        textAlign: "center"
+    },
+    header: {
+        textAlign: "center",
+        color: "gray"
+    }
+})
