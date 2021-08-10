1f9eb9c574c7d185c37890a3dd04eac1f264f4b6
nripesh <nripeshk8@gmail.com>
2021-08-10 13:07:13 +0545

    modified

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,6 +1,6 @@
 import colors from '../config/colors'
 import React,{Component} from 'react'
-import { Button, StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity , Animated, ViewComponent } from 'react-native'
+import { Button, StyleSheet, Text, View, ImageBackground, Image, TouchableWithoutFeedback , Animated, ViewComponent } from 'react-native'
 import { set, useSharedValue , useAnimatedStyle} from 'react-native-reanimated';
 
 export class SplashScreen extends Component{
@@ -8,15 +8,15 @@ export class SplashScreen extends Component{
         super(props);
         setTimeout(()=>{
             this.props.navigation.navigate("Landing");
-        },10000);
+        },6000);
     }
     state={
         animationLoading: new Animated.Value(0),
     }
     animationLoading=()=>{
         Animated.timing(this.state.animationLoading,{
-            toValue: 40,
-            duration: 9000,
+            toValue: 35,
+            duration: 5000,
             useNativeDriver: false,
         }).start();
     }
@@ -42,40 +42,46 @@ export class SplashScreen extends Component{
             extrapolate:'clamp',
         })
         var rotation=this.state.animationLoading.interpolate({
-            inputRange:[0,5,15,20,30],
+            inputRange:[0,7.5,15,22.5,30],
             outputRange:["0deg","0deg","180deg","180deg","360deg"],
             extrapolate:'clamp',
         })
         var scaling=this.state.animationLoading.interpolate({
-            inputRange:[30,40],
-            outputRange:[1,1000],
+            inputRange:[30,35],
+            outputRange:[1,700],
             extrapolateLeft:'clamp',
         })
         var tint2 = this.state.animationLoading.interpolate({
-            inputRange:[30,40],
+            inputRange:[30,35],
             outputRange:["#37c0f2","#d506fe"],
             extrapolateLeft:'clamp',
         })
+        const touch=()=>{
+            this.props.navigation.navigate("Landing")
+        }
     
         return(
             
             <View blurRadius={5} style={styles.background}>
                 <View style={styles.logoContainer}>
-                    <Animated.View style={{height:1,
-                                            width:1,
+                    <Animated.View style={{height:0.8,
+                                            width:1.5,
                                             borderRadius:1,
                                             backgroundColor:tint2,
                                             transform:[
                                                 {scale:scaling}
                                             ],}}></Animated.View>  
-                    <Animated.Image style={{width:150,
-                                            height:150,
-                                            justifyContent: "center",
-                                            alignItems: "center",
-                                            tintColor:tint1,
-                                            transform:[
-                                                {rotate:rotation}
-                                            ],}} source={require('../assets/log0.png')}></Animated.Image>
+                    <TouchableWithoutFeedback onPress={()=>touch()}>
+                        <Animated.Image style={{width:125,
+                                                height:125,
+                                                justifyContent: "center",
+                                                alignItems: "center",
+                                                tintColor:tint1,
+                                                transform:[
+                                                    {rotate:rotation}
+                                                ],}} source={require('../assets/log0.png')}></Animated.Image>
+                    </TouchableWithoutFeedback>
+                    
                 </View>
             </View>
         )
@@ -96,7 +102,6 @@ const styles = StyleSheet.create({
         bottom:260
     },
     logoContainer:{
-        position:'absolute',
         top: 10,
         alignItems:'center',
         justifyContent:'center',
